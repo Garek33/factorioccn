e2bb41d2cad6cedda7e502279aae82de5d47a7f6
Philipp Caratiola <philipp.caratiola@gmail.com>
2022-07-24 14:34:10 +0200

    Added parser.model with ModelBuilderSemantics

--- factorioccn/parser/__init__.py
+++ factorioccn/parser/__init__.py
@@ -1,7 +1,8 @@
 from tatsu import compile
 from importlib import resources
+from factorioccn.parser.model import fccnModelBuilderSemantics
 
 with resources.open_text('factorioccn.parser','grammar.ebnf') as gfile:
     _grammar = gfile.read()
 
-parser = compile(_grammar)
+parser = compile(_grammar, semantics=fccnModelBuilderSemantics())
