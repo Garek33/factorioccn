c9258f1367931c92b999cdf07dd4c5f92733b45f
Philipp Caratiola <philipp.caratiola@gmail.com>
2022-07-25 20:17:52 +0200

    use PreOrderWalker to simplify parser.stage2

--- factorioccn/parser/stage2.py
+++ factorioccn/parser/stage2.py
@@ -1,20 +1,15 @@
-from tatsu.model import NodeWalker
+from tatsu.walkers import PreOrderWalker
 
 from factorioccn.model import Circuit, Wire, DeciderCombinator, ArithmeticCombinator
 
-class FCCNWalker(NodeWalker):
+class FCCNWalker(PreOrderWalker):
     def __init__(self):
         self.circuit = Circuit()
-
-    def walk_Script(self, node):
-        for l in node.lines:
-            self.walk(l)
 
     def walk_Statement(self, node):
         if node is not None:
             node.action.input = node.input
             node.action.output = node.output
-            self.walk(node.action)
 
     def walk_Decider(self, node):
         inputs = self.processWires(node.input)
@@ -31,9 +26,6 @@ class FCCNWalker(NodeWalker):
         outputs = self.processWires(node.output)
         combinator = ArithmeticCombinator(inputs, node.left, node.op, node.right, node.result, outputs)
         self.registerCombinator(combinator)
-
-    def walk_object(self, node):
-        raise Exception(f'Unexpected node {node.__class__.__name__}')
 
     def processWires(self, wireset):
         for wire in wireset:
